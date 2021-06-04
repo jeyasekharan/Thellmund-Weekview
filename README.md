b64f6086275c4e9841af4eb982c005803fc7aedd
jeyasekharan <jeyasekharan@commusoft.com>
2021-06-04 11:33:02 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,39 @@
+Android Week View
+=================
+
+**Android Week View** is an Android library for displaying highly customizable calendar views within your app.
+
+![](images/screen-shot-new.png)
+
+Features
+---------
+* Display single-day or multi-day calendar views in your app
+* Extensive styling customization possible
+* Supports JodaTime, ThreeTenABP, and the Java 8 Date API
+* Live preview in Android Studio’s layout editor
+* Infinite horizontal scrolling
+* Interactive via click and scroll listeners
+* Written in Kotlin
+
+Getting started
+---------
+* Check out [the wiki](https://github.com/thellmund/Android-Week-View/wiki) on how to get started.
+* Take a look at the [sample app](https://github.com/thellmund/Android-Week-View/tree/develop/sample) for more details on how to use `WeekView`.
+* See the [changelog](https://github.com/thellmund/Android-Week-View/blob/develop/CHANGELOG.md) for new functionality and updates to the API.
+
+License
+----------
+
+    Copyright 2014 Raquib-ul-Alam
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ core/src/main/java/com/alamkanak/weekview/TextFitter.kt
@@ -0,0 +1,86 @@
+package com.alamkanak.weekview
+
+import android.text.SpannableStringBuilder
+import android.text.StaticLayout
+import kotlin.math.roundToInt
+
+internal class TextFitter(
+    private val viewState: ViewState
+) {
+
+    private val spannableStringBuilder = SpannableStringBuilder()
+
+    fun fit(eventChip: EventChip): StaticLayout {
+        return if (eventChip.event.isAllDay) {
+            fitAllDayEvent(eventChip)
+        } else {
+            fitSingleEvent(eventChip)
+        }
+    }
+
+    private fun fitAllDayEvent(eventChip: EventChip): StaticLayout {
+        val textPaint = viewState.getTextPaint(eventChip.event)
+        return eventChip.getText(includeSubtitle = false).toTextLayout(textPaint, width = Int.MAX_VALUE)
+    }
+
+    private fun fitSingleEvent(eventChip: EventChip): StaticLayout {
+        val bounds = eventChip.bounds
+        val availableHeight = bounds.height().roundToInt() - viewState.eventPaddingVertical * 2
+        val availableWidth = bounds.width().roundToInt() - viewState.eventPaddingHorizontal
+        return eventChip.fitText(availableWidth, availableHeight)
+    }
+
+    private fun EventChip.fitText(availableWidth: Int, availableHeight: Int): StaticLayout {
+        val textPaint = viewState.getTextPaint(event)
+
+        var text = getText(includeSubtitle = true)
+        var textLayout = text.toTextLayout(textPaint, width = availableWidth)
+
+        val fitsCompletely = textLayout.height <= availableHeight
+        if (fitsCompletely) {
+            return textLayout
+        }
+
+        while (textLayout.height > availableHeight && textLayout.lineCount > 1) {
+            // Remove the last lines until there's only a single line left. If it doesn't fit
+            // by that point, we need to reduce the text size.
+            val startOfLastLine = textLayout.getLineStart(textLayout.lineCount)
+            text = text.subSequence(startIndex = 0, endIndex = startOfLastLine - 1).trim()
+            textLayout = text.toTextLayout(textPaint, width = availableWidth)
+        }
+
+        while (textLayout.height > availableHeight && viewState.adaptiveEventTextSize) {
+            // Even a single line doesn't fit. We need to reduce the text size.
+            textPaint.textSize -= 1
+            textLayout = text.toTextLayout(textPaint, width = Int.MAX_VALUE)
+        }
+
+        return textLayout
+    }
+
+    private fun EventChip.getText(includeSubtitle: Boolean): CharSequence {
+        val subtitle = event.subtitle?.takeIf { event.isNotAllDay && includeSubtitle }
+        return combineTitleAndSubtitle(
+            title = event.title,
+            subtitle = subtitle,
+            isMultiLine = event.isNotAllDay
+        )
+    }
+
+    private fun combineTitleAndSubtitle(
+        title: CharSequence,
+        subtitle: CharSequence?,
+        isMultiLine: Boolean
+    ): CharSequence = when (subtitle) {
+        null -> title
+        else -> {
+            val separator = if (isMultiLine) "\n" else " "
+            spannableStringBuilder.clear()
+            spannableStringBuilder
+                .append(title)
+                .append(separator)
+                .append(subtitle)
+                .build()
+        }
+    }
+}
